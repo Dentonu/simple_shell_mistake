c7efd4cd0f3d41f82543f79875726c31896a7ae1
Ubuntu <ubuntu@ip-172-31-1-111.ec2.internal>
2023-05-23 22:55:58 +0000

    Okay done

--- shell.h
+++ shell.h
@@ -1,21 +1,13 @@
 #ifndef SHELL_H
 #define SHELL_H
 
-<<<<<<< HEAD
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
 
-
-void prompt(char **av, char **env);
-=======
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
->>>>>>> origin/main
+
 
 #endif /* SHELL_H */
